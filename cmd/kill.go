74b416c10922984427c9a67875129b987d7b997c
Renan DelValle <rdelval@users.noreply.github.com>
2017-12-02 15:52:56 -0800

    Added flags required for the execution of the kill command, environment, role, and name.

--- cmd/kill.go
+++ cmd/kill.go
@@ -1,17 +1,27 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
-	"fmt")
+)
+
+var env, role, name string
 
 func init() {
 	rootCmd.AddCommand(killCmd)
-  }
+	killCmd.Flags().StringVarP(&env, "environment", "e", "", "Aurora Environment")
+	killCmd.Flags().StringVarP(&role, "role", "r", "", "Aurora Role")
+	killCmd.Flags().StringVarP(&name, "name", "n", "", "Aurora Name")
+	killCmd.MarkFlagRequired("environment")
+	killCmd.MarkFlagRequired("role")
+	killCmd.MarkFlagRequired("name")
+}
 
 var killCmd = &cobra.Command{
 	Use:   "kill",
 	Short: "kill an Aurora Job",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Kill job")
+		fmt.Printf("%s %s %s", env, role, name)
 	},
-  }
+}
